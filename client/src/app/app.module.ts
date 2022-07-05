4dc404b19e126ed193a57a90dfa8ae7d915665e7
gchikovanii <gchikovanii25@gmail.com>
2022-07-05 17:02:04 +0400

    Error handling added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInteceptor } from './core/interceptors/error.inteceptor';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { HomeModule } from './home/home.module';
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInteceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/core/server-error/server-error.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-server-error',
+  templateUrl: './server-error.component.html',
+  styleUrls: ['./server-error.component.scss']
+})
+export class ServerErrorComponent implements OnInit {
+  err : any;
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.err = navigation && navigation.extras && navigation.extras.state && navigation.extras.state['error'];
+   }
+
+  ngOnInit(): void {
+  }
+
+}
