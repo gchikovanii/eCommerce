9dd4796adddc21bc96ae17e047d92368cd70b615
gchikovanii <gchikovanii25@gmail.com>
2022-07-07 10:32:56 +0400

    Registration Login and validatio client side added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component ,OnInit} from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 import { IPagination } from './shared/models/pagination';
 import { IProduct } from './shared/models/product';
@@ -12,10 +13,21 @@ import { IProduct } from './shared/models/product';
 export class AppComponent implements OnInit {
   title = 'eCommerce';
 
-  constructor(private basketService: BasketService) {
-    
+  constructor(private basketService: BasketService,private accountService: AccountService) {}
+  ngOnInit() : void {
+    this.loadBasket();
+    this.loadCurrentUser();
   }
-  ngOnInit(): void {
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+      this.accountService.loadCurrentUser(token).subscribe(() =>{
+        console.log('user loaded');
+      }, error => {
+        console.log(error);
+      })
+  }
+  loadBasket(){
     const baksetId = localStorage.getItem('basket_id');
     if(baksetId){
       this.basketService.getBasket(baksetId).subscribe(() => {
@@ -25,5 +37,5 @@ export class AppComponent implements OnInit {
       })
     }
   }
- 
+  
 }
